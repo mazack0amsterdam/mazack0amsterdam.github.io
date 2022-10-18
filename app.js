1cad0ed6bc359e2e02675ee84c4e6c88c3fc367b
maciek <72793434+mazack0amsterdam@users.noreply.github.com>
2022-10-18 09:02:18 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-document.getElementById("mail").value;
+
+window.addEventListener('DOMContentLoaded', function(e) {
+    const form = document.getElementById('formularz')
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+            var mail = document.getElementById('mailbox');
+            console.log(mail.value);
+            var haslo = document.getElementById('passwordbox');
+            console.log(haslo.value);
+
+            
+    })
+
+
+})
